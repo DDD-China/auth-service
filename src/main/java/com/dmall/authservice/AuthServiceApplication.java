237b8afb34c7e45ecc54924e95266cf0e0df3b60
Xi Qi <realxqi@gmail.com>
2018-11-28 10:13:56 +0800

    add /auth to login

--- src/main/java/com/dmall/authservice/AuthServiceApplication.java
+++ src/main/java/com/dmall/authservice/AuthServiceApplication.java
@@ -2,11 +2,29 @@ package com.dmall.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @GetMapping("/auth")
+    public Principal auth(Principal principal) {
+        return principal;
+    }
+
+    @Bean
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return HeaderHttpSessionIdResolver.xAuthToken();
+    }
 }
